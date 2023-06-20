fbc347506283d9d4df8d1cfaf770566056579e4b
Austin <56428356+HackingTrees@users.noreply.github.com>
2023-06-19 21:14:51 -0700

    Pushing existing code

---
+++ src/Database/RiotEndpoints.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class RiotEndpoints(Enum):
+    #Build out the regional endpoints
+    Americas = "americas"
+    Asia = "asia"
+    Europe = "europe"
+    #Build out the platform endpoints
+    Brazil = "bz1"
+    EuropeNorth = "eun1"
+    EuropeWest = "euw1"
+    Japan = "jp1"
+    Korea = "kr"
+    LatinAmerica = "la1"
+    LatinAmerica2 = "la2"
+    NorthAmerica = "na1"
+    Oceania = "oc1"
+    Turkey = "tk1"
+    Russia = "ru"
---
+++ src/Debugging/LogLevel.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class LogLevel(Enum):
+    debug = "[DEBUG]"
+    info = "[INFO]"
+    warning = "[WARNING]"
+    error = "[ERROR]"
